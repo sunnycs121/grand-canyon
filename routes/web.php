09861d596ccf2e395fd1acda9207b18851944299
Hassan Farooq <hassan.farooq121@gmail.com>
2023-02-21 22:18:36 -0500

    Reverting to default laravel home page

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,13 @@ use App\Http\Controllers\GceUsersController;
 */
 
 Route::get('/', function () {
-    return to_route('dashboard');
-})->middleware(['auth', 'verified']);
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
 
 Route::get('/dashboard', [GceUsersController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
